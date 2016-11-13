9fe9698562a207040c85aa7ceb9f66b416b469ad
ifeanyiisitor <ifeanyiisitor@gmail.com>
2016-11-13 18:26:44 +0000

    add more tests

--- src/Block.js
+++ src/Block.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import toClassNames from 'classnames'
 
-//import './Block.scss' // TODO: Uncomment
+// import './Block.scss' // TODO: Uncomment
 
 export default class Block extends React.Component {
 
